999987492a50ea1362081706743b543a74aa700c
masoudvrz <masoud.varzi@yahoo.com>
2023-12-12 12:28:14 +0330

    feat: implemented storeProvider for theme mode

---
+++ app/redux/StoreProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { FC, ReactNode, useEffect } from "react";
+import { Provider, useSelector } from "react-redux";
+import store from "./store";
+import { getThemeMode } from "./slice";
+
+type ProviderPropsType = {
+  children: ReactNode;
+};
+
+const ThemeProvider: FC<ProviderPropsType> = ({ children }) => {
+  const theme = useSelector(getThemeMode);
+
+  useEffect(() => {
+    document.body.dataset.theme = theme;
+  }, [theme]);
+
+  return <>{children}</>;
+};
+
+const StoreProvider: FC<ProviderPropsType> = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </Provider>
+  );
+};
+
+export default StoreProvider;
