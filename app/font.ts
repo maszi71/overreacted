a03d9d6c589404df70f5e2a034c3f69124918a88
masoudvrz <masoud.varzi@yahoo.com>
2023-12-12 13:25:33 +0330

    feat: added fonts to app

---
+++ app/font.ts
@@ -0,0 +1,14 @@
+import { Montserrat, Merriweather } from 'next/font/google'
+
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700', '900'],
+})
+
+export const merriweather = Merriweather({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700' , '900'],
+})
