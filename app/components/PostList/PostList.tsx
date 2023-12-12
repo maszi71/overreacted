64603901b904be9fd18de93463845d7e94ddc924
masoudvrz <masoud.varzi@yahoo.com>
2023-12-12 13:07:37 +0330

    refactor: updating postList

--- app/components/PostList/PostList.tsx
+++ app/components/PostList/PostList.tsx
@@ -11,12 +11,11 @@ type PostListType = {
 const PostList: FC<PostListType> = (props) => {
   const { list } = props;
   const { observerReference, visibleItems } = useInfiniteScroll(list, 10);
-  console.log(visibleItems)
 
   return (
     <div>
       {visibleItems.map((post: PostType, index: number) => (
-        <Item key={post.id} item={post} index={index} />
+        <Item key={post.id} item={post} index={index} total={list.length} />
       ))}
       <div style={{ height: "2.5rem" }} ref={observerReference} />
     </div>
